aedce94caa166cc69431a55c1615b594eae2736f
Akshanabha <84524025+ahqsoftwares@users.noreply.github.com>
2022-09-19 21:27:40 +0530

    publisher

---
+++ publish.js
@@ -0,0 +1,26 @@
+const { exec } = require("child_process");
+
+const version = require("./package.json").version;
+const chalk = require("chalk");
+
+function publish(tag) {
+         console.log(`Publishing @${chalk.green(tag ? tag : "latest")}`);
+         exec(`npm publish${tag ? `--tag=${tag}` : ""}`, {env: process.env}, (err, stderr, out) => {
+                  if (err) {
+                           throw new Error("Unable to run command!");
+                  }
+                  if (stderr) {
+                           console.log(chalk.red(String(stderr)));
+                           return
+                  }
+                  console.log(chalk.green(String(out)));
+         });
+}
+
+if (version.endsWith("-dev")) {
+         publish("dev");
+} else if (version.endsWith("--beta")) {
+         publish("beta");
+} else {
+         publish();
+}
